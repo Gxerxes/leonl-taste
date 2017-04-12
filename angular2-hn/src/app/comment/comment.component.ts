295abddd351b2725933c876c63e7e97e58b0b876
xuninglee <xuninglee@163.com>
2017-04-12 15:55:22 +0800

    deploy to firebase

--- angular2-hn/src/app/comment/comment.component.ts
+++ angular2-hn/src/app/comment/comment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-comment',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentComponent implements OnInit {
 
+  @Input() comment;
+  collapse: boolean;
+
   constructor() { }
 
   ngOnInit() {
+    this.collapse = false;
   }
 
 }
